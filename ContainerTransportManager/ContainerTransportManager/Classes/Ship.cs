61a83a1c8d301c9a741de46c5d611690bb22737e
Unknown <calvindemeester@gmail.com>
2018-12-20 22:29:07 +0100

    Added CreateContainerPiles function

--- ContainerTransportManager/ContainerTransportManager/Classes/Ship.cs
+++ ContainerTransportManager/ContainerTransportManager/Classes/Ship.cs
@@ -17,5 +17,36 @@ namespace ContainerTransportManager.Classes
 
         }
 
+        public List<ContainerPile> SortContainersInPiles()
+        {
+
+        }
+
+        public List<ContainerPile> CreateContainerPiles(int maxcolumns, int maxrows)
+        {
+            List<ContainerPile> containerPiles = new List<ContainerPile>();
+            int containerId = 0;
+            for (int x = 0; x < maxcolumns; x++)
+            {
+                ShipSide side = ShipSide.Middle;
+                int numberOfHalf = maxcolumns / 2;
+                if (x < numberOfHalf)
+                {
+                    side = ShipSide.Left;
+                }
+                else if (x > numberOfHalf)
+                {
+                    side = ShipSide.Right;
+                }
+                for (int y = 0; y < maxrows; y++)
+                {
+                    ContainerPile containerPileWithPosition = new ContainerPile(containerId, x, y, side);
+                    containerPiles.Add(containerPileWithPosition);
+                    containerId++;
+                }
+            }
+            return containerPiles;
+        }
+
     }
 }
