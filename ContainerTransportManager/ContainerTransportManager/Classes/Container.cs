b296b2a699d562056aa866d188a7fea9ca9271c2
Unknown <calvindemeester@gmail.com>
2018-12-20 22:14:21 +0100

    Added properties to Container.cs

--- ContainerTransportManager/ContainerTransportManager/Classes/Container.cs
+++ ContainerTransportManager/ContainerTransportManager/Classes/Container.cs
@@ -6,7 +6,25 @@ using System.Threading.Tasks;
 
 namespace ContainerTransportManager.Classes
 {
-    class Container
+    public class Container
     {
+        public enum ContainerType
+        {
+            Regular,
+            Valuable,
+            Cooled,
+        }
+        public int ContainerId { get; set; }
+        public double Weight { get; set; } = 4000;
+        public ContainerType Type { get; set; } = ContainerType.Regular;
+        public int PilePosition { get; set; }
+
+        public Container(int containerid, int weight, ContainerType type, int pileposition)
+        {
+            ContainerId = containerid;
+            Weight = weight;
+            Type = type;
+            PilePosition = pileposition;
+        }
     }
 }
